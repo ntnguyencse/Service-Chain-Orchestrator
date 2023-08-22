4211c55ff66358dfdd09c939a196d316379bbdd1
Thanh Nguyen <ntnguyencse@gmail.com>
2023-08-22 23:45:55 +0900

    Add gen gateway script

--- hack/install-istio.sh
+++ hack/install-istio.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.17.5 TARGET_ARCH=x86_64 sh -
-istioctl operator init
+# istioctl operator init
 cd istio-${ISTIO_VERSION}
 export PATH=$PWD/bin:$PATH
 # export KUBECONFIG=
 ./bin/istioctl operator init
+
+
+
+samples/multicluster/gen-eastwest-gateway.sh \
+    --mesh mesh1 --cluster cluster1 --network network1 | \
+    istioctl --context="${CTX_CLUSTER1}" install -y -f -
